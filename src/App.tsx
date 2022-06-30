76ab403f3cc84908c250f63b57d531596dc47d29
Gaston <gaston.camano.gc@gmail.com>
2022-06-30 20:23:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App ()
+{
+    return (
+        <div className="App">
+            <div className='content'>
+                <nav></nav>
+                <header className="App-header"></header>
+                <section></section>
+            </div>
+        </div>
+    );
+}
+
+export default App;
